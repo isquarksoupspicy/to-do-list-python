f1b6fcd09e8f87fc4ff68b200f89725b5b2fa4c4
Srishti <srishti.anya@gmail.com>
2026-09-21 22:51:54 +0530

    add menu loop with quit option

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    print("1. Add task")
+    print("2, view tasks")
+    print("3. Quit")
+    choice = input("Choose an option: ")
+    if choice == "3":
+        print("goodbye!")
+        break
+    print("You picked", choice)
