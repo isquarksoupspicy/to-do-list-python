20e55b12195921ac8ce620d4beb34cc7de544f49
Srishti <srishti.anya@gmail.com>
2026-09-22 10:37:00 +0530

    add task functionally with list storage

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+tasks = []
+
 while True:
     print("1. Add task")
-    print("2, view tasks")
+    print("2. View tasks")
     print("3. Quit")
     choice = input("Choose an option: ")
-    if choice == "3":
-        print("goodbye!")
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        print(tasks)
+    elif choice == "3":
+        print("Goodbye! ")
         break
-    print("You picked", choice)
